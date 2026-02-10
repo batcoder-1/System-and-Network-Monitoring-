2dfe617b787ea149722d225d98037a07ef5424c3
Naman Dadhich <ndmajor2005@gmail.com>
2026-02-11 03:08:45 +0530

    updated monitor.sh.Added help and networking section

--- montior.sh
+++ montior.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 # this script is used for networking monitoring along with system monitoring
-
+print_star(){
+         echo "================================================"
+}
 error_check(){
 if [ $? != 0 ]
 then
-        echo "=============================================="
+        print_star
         echo "Error occured kindly look the error logs for more information"
-        echo "=============================================="
+	print_star
         exit 1
 fi
 }
 echo "Welcome $(whoami)"
 error_check
-if [ "$1" = '-s' ]
+if [ "$1" == '-s' ]
 then
 echo "System uptime is: $(uptime -p)"
 error_check
 echo "Number of users are: $(uptime | cut -d "," -f 2)"
 error_check
+#------------------------------------------------------
+elif [ "$1" == '-n' ]
+then
+	echo "networking monitoring"
+
+#------------------------------------------------------
+elif [ "$1" == '-h' ]
+then 
+	echo "help section"
+#-------------------------------------------------------
+else
+	print_star
+	echo "Please select  one option. Available options are:
+	      1) -s for system monitoring 
+	      2) -n for network monitoring
+	      3) -h for help"
+       	print_star
 fi
