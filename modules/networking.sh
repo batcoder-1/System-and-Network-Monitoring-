aa693cc9cbc7587ccf8b1f8fa151c2ffb1b1d5e9
Naman Dadhich <ndmajor2005@gmail.com>
2026-02-18 02:16:53 +0530

    Defined structure of networking module and implemented layer 1 of netcheck in network module

--- modules/networking.sh
+++ modules/networking.sh
@@ -1,23 +1,49 @@
-#---------------------------------------------------------------
-#functions
-print_star(){
-         echo "================================================"
-}
-error_check(){
-if [ $? != 0 ]
-then
+        # this primarily consist of netcheck + netinfo + ports and address info 
+        #network.sh
+        #       -> netcheck()
+        #       -> netinfo()
+        #       -> netports()
+        # # netcheck logic:
+        # 1. detect active interface
+        # 2. verify IP
+        # 3. check default route
+        # 4. test connectivity
+        # 5. test DNS
+
+        #---------------------------------------------------------------
+        #functions
+        print_star(){
+                echo "================================================"
+        }
+        error_check(){
+        if [ $? != 0 ]
+        then
+                print_star
+                echo "Error occured kindly look the error logs for more information"
+                print_star
+                exit 1
+        fi
+        }
+        print_line(){
+                echo "$1 section" 
+                print_star
+        }
+        netcheck(){
+                Number_of_interface=$(ls /sys/class/net | wc -l);
+                echo "Number of network interfaces available: $Number_of_interface"
+                interfaces=$(ls /sys/class/net);
+                for interface in $interfaces;
+                        do kernel_interface_status=$(cat /sys/class/net/$interface/operstate);
+                           physical_interface_status=$(cat /sys/class/net/$interface/carrier);
+                           if [ $kernel_interface_status == "up" ] && [ $physical_interface_status == "1" ]
+                                then echo "Interface $interface:link-ready to use"
+                           else
+                                echo "Interface $interface: not link-ready"
+                           fi
+                        done;
+        }
+        #-------------------------------------------------------
+        print_line Networking
+        netcheck 
         print_star
-        echo "Error occured kindly look the error logs for more information"
-	print_star
-        exit 1
-fi
-}
-print_line(){
-	echo "$1 section" 
-	print_star
-}
-#-------------------------------------------------------
-print_line Networking
-echo "Netoworking Section"
-print_star
-#-------------------------------------------------------
+        #-------------------------------------------------------
