f02cdc33aa12247ae0182a0317cf044d215a6dce
Naman Dadhich <ndmajor2005@gmail.com>
2026-02-18 17:10:38 +0530

    Added ip assign check and link check for network interfaces

--- modules/networking.sh
+++ modules/networking.sh
@@ -28,6 +28,10 @@
                 echo "$1 section" 
                 print_star
         }
+        check_ip4(){
+                ip addr show $1 | grep "inet" | grep -v "inet6" >> /dev/null
+                return $?;
+        }
         netcheck(){
                 Number_of_interface=$(ls /sys/class/net | wc -l);
                 echo "Number of network interfaces available: $Number_of_interface"
@@ -36,11 +40,18 @@
                         do kernel_interface_status=$(cat /sys/class/net/$interface/operstate);
                            physical_interface_status=$(cat /sys/class/net/$interface/carrier);
                            if [ $kernel_interface_status == "up" ] && [ $physical_interface_status == "1" ]
-                                then echo "Interface $interface:link-ready to use"
+                                then echo "Interface $interface:link-ready to use";
+                                check_ip4 $interface
+                                if [ $? -eq 0 ]
+                                        then echo "Interface $interface is ip configured"
+                                else
+                                        echo "Interface $interface is not ip configured"
+                                fi
                            else
                                 echo "Interface $interface: not link-ready"
                            fi
                         done;
+                        
         }
         #-------------------------------------------------------
         print_line Networking
